a21c7b1bfab950e22bf03109f0f3bae8c37eaeb3
Rui Cai <rianddy@gmail.com>
2014-11-16 21:36:06 -0500

    Merge permission-ruibi client

--- CourseManagementClient/src/java/team/soa/cmsc/servlet/faculty/CheckPermsReqServlet.java
+++ CourseManagementClient/src/java/team/soa/cmsc/servlet/faculty/CheckPermsReqServlet.java
@@ -7,10 +7,14 @@ package team.soa.cmsc.servlet.faculty;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.jms.Message;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import team.soa.cmsc.msgQueueHandler.MsgQueueReceiver;
 
 /**
  *
@@ -30,8 +34,11 @@ public class CheckPermsReqServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String permsId=request.getParameter("permsId");
-        
+        String permsId = request.getParameter("permsId");
+
+        MsgQueueReceiver receiver = new MsgQueueReceiver("stuclassqcf", "stuclassqueue");
+        List<Message> msgList = new ArrayList<Message>();
+        msgList = receiver.browserMessage();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
