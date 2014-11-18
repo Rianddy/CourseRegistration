5a74d5abcb20645ffefb07f7040e19a20378f88d
xmruibi <xmruibi@gmail.com>
2014-11-18 15:47:27 -0500

    msg queue with mail #5

--- CourseManagementClient/src/java/team/soa/cmsc/servlet/faculty/CheckPermsReqServlet.java
+++ CourseManagementClient/src/java/team/soa/cmsc/servlet/faculty/CheckPermsReqServlet.java
@@ -14,6 +14,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+import team.soa.cms.msgqueueservice.PermsReqMsgService_Service;
+import team.soa.cms.msgqueueservice.StuClassInfoMQSerialObj;
 
 
 /**
@@ -21,6 +24,8 @@ import javax.servlet.http.HttpServletResponse;
  * @author birui
  */
 public class CheckPermsReqServlet extends HttpServlet {
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/CourseManagementSystem/PermsReqMsgService.wsdl")
+    private PermsReqMsgService_Service service;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,8 +40,27 @@ public class CheckPermsReqServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String permsId = request.getParameter("permsId");
-
+        List<StuClassInfoMQSerialObj> permsMsgList = new  ArrayList<StuClassInfoMQSerialObj>();
+        permsMsgList = getPermsInfo(permsId);
+        request.setAttribute("PermsMsgList", permsMsgList);
         
+        /**************************************************Test**********************************************/
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Faculty check Permission Request</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Permission Request from " + permsMsgList.get(0).getStuname()+ "</h1>");
+            out.println("<h3>Request Permission for " + permsMsgList.get(0).getRequestclasslist().get(0).getCoursename() + "</h3>");
+            out.println("<h3>Accept </h3>");
+            out.println("<h3>Rejuct </h3>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        /***************************************************************************************************/
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -78,4 +102,12 @@ public class CheckPermsReqServlet extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    private java.util.List<team.soa.cms.msgqueueservice.StuClassInfoMQSerialObj> getPermsInfo(java.lang.String permsID) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        team.soa.cms.msgqueueservice.PermsReqMsgService port = service.getPermsReqMsgServicePort();
+        return port.getPermsInfo(permsID);
+    }
+
+    
 }
