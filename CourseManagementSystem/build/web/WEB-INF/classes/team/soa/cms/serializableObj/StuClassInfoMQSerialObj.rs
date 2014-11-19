b785403854d17d3a16ed9ec12ea60ae218e6d2b0
xmruibi <xmruibi@gmail.com>
2014-11-18 22:59:01 -0500

    Merge branch 'RuiB'

--- CourseManagementSystem/build/web/WEB-INF/classes/team/soa/cms/serializableObj/StuClassInfoMQSerialObj.rs
+++
@@ -1,4 +0,0 @@
-team.soa.cms.serializableObj.Requestclassinfo
-team.soa.cms.serializableObj.StuClassInfoMQSerialObj
-team.soa.cms.ws.serializableObj.Requestclassinfo
-team.soa.cms.ws.serializableObj.StuClassInfoMQSerialObj
